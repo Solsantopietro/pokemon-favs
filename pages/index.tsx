b5dca5ef43c3b6a8db23c9bffd29b0364b49952d
sol <sol@numbisca.club>
2023-06-05 14:24:14 -0300

    request todos los pokemones

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,35 @@
+import { NextPage, GetStaticProps } from 'next'
 import { Button } from '@nextui-org/react'
-import { NextPage } from 'next'
 import { Layout } from '@/components/layouts'
+import { pokeApi } from '@/api'
 
 
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props)
   return (
     <Layout title='Pokemon App | Home'>
-      <Button color='gradient'>Hello world!</Button>
+    <ul>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+    </ul>
     </Layout>
+
   )
+}
+
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const resp = await pokeApi.get('/pokemon?limit=151');
+ console.log({resp})
+
+  return {
+    props: {},
+  }
 }
 
 export default HomePage
